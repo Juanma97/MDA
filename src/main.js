2aa2e8bb4c8a0ec0b784536cbdf2e207cb918a96
vidaxd <>
2019-03-14 17:43:42 +0000

    init firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import './plugins/vuetify'
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import firebase from 'firebase';
+import Vuetify from 'vuetify';
+
+ // Initialize Firebase
+ const config = {
+  apiKey: "AIzaSyDyFjixJxxSNBmXqusJ8COmkju2pG8NB5I",
+  authDomain: "practica-mda.firebaseapp.com",
+  databaseURL: "https://practica-mda.firebaseio.com",
+  projectId: "practica-mda",
+  storageBucket: "practica-mda.appspot.com",
+  messagingSenderId: "943532623760"
+};
+
+const appDB = firebase.initializeApp(config);
+
+export const db = appDB.database();
+
+Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
 
@@ -11,3 +29,5 @@ new Vue({
   store,
   render: h => h(App),
 }).$mount('#app');
+
+ 
