1d11110b7c12defc0e1bbcb89e1632c476986935
vidaxd <>
2019-03-19 12:50:16 +0000

    dummy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,15 @@
 <template>
   <div class="main">
-    <v-toolbar color="red">
-
-    </v-toolbar>
+    <v-toolbar color="#3498db">
+    <v-btn outline fab>
+      <v-icon x-large dark>home</v-icon>
+    </v-btn>
+    <v-btn >
+      
+    </v-btn>
+    <v-spacer></v-spacer>
+    
+  </v-toolbar>
   </div>
 </template>
 
