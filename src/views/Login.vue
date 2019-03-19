8e6d1720ccdeaddcb23336f092fed3f8c2e75735
Juan Manuel Perez Toro <juanma97@outlook.com>
2019-03-19 13:52:15 +0000

    Login and register done

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="main">
+    <v-icon @click="goToMainScreen" color="white">arrow_back</v-icon> 
     <div class="container-login">
+
+      <img src="../assets/login.png" />
+      <p class="title-form" v-if="!showRegisterForm">Sign in</p>
+      <p class="title-form" v-if="showRegisterForm">Sign up</p>
 
       <v-card v-if="!showRegisterForm">
         <v-text-field
@@ -15,10 +20,10 @@
           append-icon="lock"
           type="password"
         ></v-text-field>
-        <v-btn @click="login">
+        <v-btn outline color="#3498db" @click="login">
           Login
         </v-btn>
-        <v-btn @click="showRegisterForm = !showRegisterForm" outline>Register</v-btn>
+        <p @click="showRegisterForm = !showRegisterForm">Don't have a account?</p>
       </v-card>
 
       <v-card v-if="showRegisterForm">
@@ -39,12 +44,18 @@
           append-icon="lock"
           type="password"
         ></v-text-field>
-        <v-btn @click="register">
+        <v-btn color="#3498db" outline @click="register">
           Register
         </v-btn>
-        <v-btn @click="showRegisterForm = !showRegisterForm" outline>Login</v-btn>
+        <p @click="showRegisterForm = !showRegisterForm">Already have a account?</p>
       </v-card>
 
+    </div>
+    <div class="footer">
+      <p>About us</p>
+      <p>Our clients</p>
+      <p>Why select this enterprise?</p>
+      <p class="copyright">Copyright 2019</p>
     </div>
   </div>
 </template>
@@ -63,6 +74,9 @@ export default {
     }
   },
   methods: {
+    goToMainScreen(){
+      this.$router.replace('/');
+    },
     login (){
       firebase.auth().signInWithEmailAndPassword(this.email, this.password)
       .then((result) => {
@@ -91,11 +105,35 @@ export default {
   width: 100%;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background: #3498db;
+  position: relative;
+}
+.footer{
+  border-top: 1px solid white;
+  display: flex;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
 }
 
+.footer p{
+  margin: 16px !important;
+  transition: 400ms;
+}
+
+.footer p:hover{
+  font-size: 18px;
+  color: white;
+}
+.v-icon{
+ position: absolute;
+ left: 16px;
+ top: 16px;
+ font-size: 48px;
+}
 .container-login{
   display: flex;
   flex-direction: column;
@@ -110,6 +148,32 @@ export default {
   padding: 16px;
 }
 
+p{
+  margin: 16px 0;
+  text-align: center;
+}
+
+p:hover{
+  cursor: pointer;
+}
+
+.v-btn{
+  padding: 0 !important;
+  margin: 0 !important;
+  width: 100%;
+  justify-self: center;
+}
+
+
+.title-form{
+  font-size: 24px;
+  color: white;
+}
+
+img{
+  width: 120px;
+  height: 120px;
+}
 </style>
 
 
