e6c862bae94efe1906c330bf65028a500da8a9a0
Juan Manuel Perez Toro <juanma97@outlook.com>
2019-04-09 13:04:42 +0100

    Buy product done

--- src/views/DetailsProducts.vue
+++ src/views/DetailsProducts.vue
@@ -1,7 +1,7 @@
 <template>
-    <div style="position: relative; height: 100vh;">
+    <div style="position: relative; height: 100%; display: flex; align-items: center; flex-direction:column;">
     <ToolbarComponent />  
-        <v-carousel height="400">
+        <v-carousel height="300">
             <v-carousel-item
             :src="this.item.image1"
             reverse-transition="fade"
@@ -18,27 +18,28 @@
             transition="fade"
             ></v-carousel-item>
         </v-carousel>
-        <table  border= "1" id="mitabla">
-            <tr>
-                <th class='text'>Name</th>
-                <th class='text'>Price</th>
-                <th class='text'>Quantity</th>
-                <th class='text'>Description</th>
-                <th class='text'>Seller</th>
-            </tr>
-            <tbody>
-            <tr>
-                <td>{{item.nameProduct}}</td>
-                <td>{{item.priceProduct}}</td>
-                <td>{{item.quantityProduct}}</td>
-                <td>{{item.descriptionProduct}}</td>
-                <td>{{nameSeller}}</td>
-            </tr> 
-            </tbody>
-
-        </table>
+        <div class="details">
+            <h3>Name product</h3>
+            <p>{{item.nameProduct}}</p>
+            <h3>Description product</h3>
+            <p>{{item.descriptionProduct}}</p>
+            <h3>Price product</h3>
+            <p>{{item.priceProduct}}</p>
+            <h3>Name seller</h3>
+            <p>{{nameSeller}}</p>
+        </div>
+        <div v-if="showForm" class="buy-form">
+            <h3>Formulario de compra</h3>
+            <v-text-field v-if="showForm" v-model="name" placeholder="name"></v-text-field>
+            <v-text-field v-if="showForm" v-model="lastname" placeholder="lastname"></v-text-field>
+            <v-text-field v-if="showForm" v-model="direction" placeholder="direction"></v-text-field>
+            <v-text-field v-if="showForm" v-model="email" placeholder="email"></v-text-field>
+            <v-text-field v-if="showForm" v-model="card_number" placeholder="card_number"></v-text-field>
+            <v-btn outline @click="buyProduct(item)" v-if="showForm">Finalizar compra</v-btn>
+            <v-btn outline @click="hideForm()" v-if="showForm">Cancelar compra</v-btn>
+        </div>
         <div class="buy-btn">
-            <v-btn outline @click="buyProduct(item)">Comprar producto</v-btn>
+            <v-btn outline @click="showFormBuy()" v-if="!showForm">Comprar producto</v-btn>
             <v-alert type="success" dismissible :value="buySuccessful">Producto comprado</v-alert>
             <v-alert type="error" dismissible :value="errorLogin">Tiene que registrarse para comprar</v-alert>
         </div>
@@ -60,6 +61,8 @@ export default ({
         var ref = firebase.database().ref('/users/'+this.item.uid)
         ref.once('value', (snapshot) => {
             this.nameSeller = snapshot.val().name;
+        }).then(() => {
+            this.getUserInformation();
         })
     },
    data () {
@@ -67,16 +70,49 @@ export default ({
        item: null,
        buySuccessful: false,
        errorLogin: false,
+       showForm: true,
        nameSeller: '',
+       name: '',
+       lastname: '',
+       card_number: '',
+       direction: '',
+       email: '',
     }
   },
   methods: {
+      getUserInformation() {
+          var ref = firebase.database().ref('/users/'+firebase.auth().currentUser.uid);
+          ref.once('value', (snapshot) => {
+              this.name = snapshot.val().name;
+              this.lastname = snapshot.val().lastname;
+              this.email = snapshot.val().email;
+          })
+      },
+      showFormBuy() {
+          if(firebase.auth().currentUser != null){
+              this.showForm = true;
+          }else{
+              this.errorLogin = true;
+          }
+      },
+      hideForm() {
+          this.showForm = false;
+      },
       buyProduct(item){
           if(firebase.auth().currentUser != null){
               var ref = firebase.database().ref('historial_compras/'+firebase.auth().currentUser.uid)
-              ref.push(item)
+              ref.push({
+                  item: item,
+                  nameSeller: this.nameSeller,
+                  direction: this.direction,
+                  card_number: this.card_number,
+                  name: this.name,
+                  lastname: this.lastname,
+                  email: this.email,
+                })
               .then(() => {
                   this.buySuccessful = true;
+                  this.showForm = false;
               })
               .catch(() => {
                   this.errorLogin = true;
@@ -91,6 +127,15 @@ export default ({
 </script>
 
 <style >
+.buy-form{
+    width: 70%;
+
+}
+.details{
+    align-self: left !important;
+    width: 100%;
+    padding: 32px;
+}
     .buy-btn{
         position: absolute;
         bottom: 0;
